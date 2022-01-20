d9d045e4fbd78d28b8cacf5d6c9175352b2caf14
Stanislav Buinovsky <sbuinovsky@gmail.com>
2022-01-20 13:23:44 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Станислав Буйновский on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
